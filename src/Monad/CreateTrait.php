ca219cb939d1847dc83844b417470cf6bf6ea443
widmogrod <widmogrod@gmail.com>
2015-02-28 19:25:23 +0100

    Improve autocomplete

--- src/Monad/CreateTrait.php
+++ src/Monad/CreateTrait.php
@@ -7,7 +7,7 @@ trait CreateTrait
      * Convert value to monad
      *
      * @param mixed $value
-     * @return MonadInterface|self
+     * @return MonadInterface|LiftInterface
      */
     public static function create($value)
     {
