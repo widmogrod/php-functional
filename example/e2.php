be96d69dfaf2aeaff9cea8d039d6c340d51745de
widmogrod <widmogrod@gmail.com>
2015-03-16 19:49:37 +0100

    add two more examples

---
+++ example/e2.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Monad\Either;
+use Monad\Utils;
+
+function read($file)
+{
+    return is_file($file)
+        ? Either\Right::create(file_get_contents($file))
+        : Either\Left::create(sprintf('File "%s" does not exists', $file));
+}
+
+$concat = Utils::liftM2(
+    read(__DIR__ . '/e1.php'),
+    read('aaa'),
+    function ($first, $second) {
+        return $first . $second;
+    }
+);
+
+assert($concat instanceof Either\Left);
+assert($concat->orElse(Utils::returns) === 'File "aaa" does not exists');
