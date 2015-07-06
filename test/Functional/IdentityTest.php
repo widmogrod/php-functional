0a69961831b8bbe412f0ce262c80391983802207
widmogrod <widmogrod@gmail.com>
2015-07-06 11:14:12 +0200

    test identity

---
+++ test/Functional/IdentityTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace test\Functional;
+
+use Functional as f;
+
+class IdentityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function test_it_should_return_given_value(
+        $value
+    ) {
+        $this->assertEquals($value, f\identity($value));
+    }
+
+    public function provideData()
+    {
+        return [
+            'integer' => [
+                '$value' => 1,
+            ],
+            'string' => [
+                '$value' => 'bar',
+            ],
+            'list' => [
+                '$value' => ['bar', 'baz'],
+            ],
+            'map' => [
+                '$value' => ['x' => 'bar', 'y' => 'baz'],
+            ],
+        ];
+    }
+}
