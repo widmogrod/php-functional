0c4242d4f4ab4fd2c2e3fa939d0d60812aa065fb
widmogrod <widmogrod@gmail.com>
2015-08-23 19:49:29 +0200

    add type signature for ChainInterface::bind

--- src/FantasyLand/ChainInterface.php
+++ src/FantasyLand/ChainInterface.php
@@ -4,6 +4,9 @@ namespace FantasyLand;
 interface ChainInterface extends ApplyInterface
 {
     /**
+     * bind :: Monad m => (a -> m b) -> m b
+     *
+     * @return mixed|\Closure
      * @param callable $function
      * @return self
      */
