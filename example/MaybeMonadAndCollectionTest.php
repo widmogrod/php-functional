eb05ddb87df5babf5e815e14d92142bc8b24e1ad
widmogrod <widmogrod@gmail.com>
2015-09-12 16:33:27 +0200

    new example for maybe monad and collection monad

--- example/MaybeMonadAndCollectionTest.php
+++ example/MaybeMonadAndCollectionTest.php
@@ -7,14 +7,11 @@ use Functional as f;
 
 class MaybeMonadAndCollectionTest extends \PHPUnit_Framework_TestCase
 {
-    public function test_it_should_extract_elements_which_exists()
+    /**
+     * @dataProvider provideData
+     */
+    public function test_it_should_extract_elements_which_exists($data)
     {
-        $data = [
-            ['id' => 1, 'meta' => ['images' => ['//first.jpg', '//second.jpg']]],
-            ['id' => 2, 'meta' => ['images' => ['//third.jpg']]],
-            ['id' => 3],
-        ];
-
         // $get :: String a -> [b] -> Maybe b
         $get = f\curryN(2, function ($key, $array) {
             return isset($array[$key])
@@ -38,5 +35,46 @@ class MaybeMonadAndCollectionTest extends \PHPUnit_Framework_TestCase
             ['//first.jpg', '//third.jpg', null],
             $result
         );
+    }
+
+    /**
+     * @dataProvider provideData
+     */
+    public function test_it_should_extract_elements_which_exists_alternative_solution($data)
+    {
+        // $get :: String a -> Maybe [b] -> Maybe b
+        $get = function ($key) {
+            return f\bind(function ($array) use ($key) {
+                return isset($array[$key])
+                    ? Maybe\just($array[$key])
+                    : Maybe\nothing();
+            });
+        };
+
+        $result = Collection::of($data)
+            ->map(Maybe\maybeNull)
+            ->bind($get('meta'))
+            ->bind($get('images'))
+            ->bind($get(0));
+
+        $result = f\valueOf($result);
+
+        $this->assertEquals(
+            ['//first.jpg', '//third.jpg', null],
+            $result
+        );
+    }
+
+    public function provideData()
+    {
+        return [
+            'default' => [
+                '$data' => [
+                    ['id' => 1, 'meta' => ['images' => ['//first.jpg', '//second.jpg']]],
+                    ['id' => 2, 'meta' => ['images' => ['//third.jpg']]],
+                    ['id' => 3],
+                ]
+            ],
+        ];
     }
 }
