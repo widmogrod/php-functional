c96233a36979c56148defdbf5fdad1c70e01faf4
widmogrod <widmogrod@gmail.com>
2016-10-22 01:23:18 +0200

    Introduce Sum primitive type + tests

---
+++ src/Primitive/Sum.php
@@ -0,0 +1,40 @@
+<?php
+namespace Widmogrod\Primitive;
+
+use Widmogrod\Common;
+use Widmogrod\FantasyLand;
+
+class Sum extends Num implements
+    FantasyLand\Monoid,
+    FantasyLand\Pointed
+{
+    use Common\PointedTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public static function mempty()
+    {
+        return self::of(0);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEmpty()
+    {
+        return self::mempty();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function concat(FantasyLand\Semigroup $value)
+    {
+        if ($value instanceof self) {
+            return self::of($this->extract() + $value->extract());
+        }
+
+        throw new TypeMismatchError($value, self::class);
+    }
+}
---
+++ test/Primitive/SumTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace test\Primitive;
+
+use Widmogrod\Functional as f;
+use Widmogrod\Helpful\MonoidLaws;
+use Widmogrod\Helpful\SetoidLaws;
+use Widmogrod\Primitive\Sum;
+
+class SumTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideSetoidLaws
+     */
+    public function test_it_should_obay_setoid_laws(
+        $a,
+        $b,
+        $c
+    ) {
+        SetoidLaws::test(
+            f\curryN(3, [$this, 'assertEquals']),
+            $a, $b, $c
+        );
+    }
+
+    /**
+     * @dataProvider provideSetoidLaws
+     */
+    public function test_it_should_obay_monoid_laws(
+        $a,
+        $b,
+        $c
+    ) {
+        MonoidLaws::test(
+            f\curryN(3, [$this, 'assertEquals']),
+            $a, $b, $c
+        );
+    }
+
+    private function randomize()
+    {
+        return Sum::of(rand(-100000000, 100000000));
+    }
+
+    public function provideSetoidLaws()
+    {
+        return array_map(function () {
+            return [
+                $this->randomize(),
+                $this->randomize(),
+                $this->randomize(),
+            ];
+        }, array_fill(0, 50, null));
+    }
+}
