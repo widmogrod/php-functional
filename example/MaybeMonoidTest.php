40c7592f80bfcc1d06bbe9eca992e23e8600cc6b
Thomas Ploch <thomas.ploch@flixbus.com>
2017-12-12 18:32:57 +0100

    Apply coding standard fixes for namespace imports

--- example/MaybeMonoidTest.php
+++ example/MaybeMonoidTest.php
@@ -1,6 +1,7 @@
 <?php
 
-use Widmogrod\Monad\Maybe\Just;
+use Widmogrod\Functional as f;
+use Widmogrod\Monad\Maybe as m;
 use Widmogrod\Primitive\Listt;
 
 class MaybeMonoidTest extends \PHPUnit_Framework_TestCase
@@ -11,7 +12,7 @@ class MaybeMonoidTest extends \PHPUnit_Framework_TestCase
     public function test_it_should_concat_only_just_values($data, $expected)
     {
         $makeMaybeMonoid = function ($val) {
-            return \Widmogrod\Monad\Maybe\maybeNull($val)->map(Listt::of);
+            return m\maybeNull($val)->map(Listt::of);
         };
 
         $names = array_values(array_map($makeMaybeMonoid, $data));
@@ -19,7 +20,7 @@ class MaybeMonoidTest extends \PHPUnit_Framework_TestCase
         list($firstName, $middleName, $lastName) = $names;
 
         $fullName = $firstName->concat($middleName)->concat($lastName);
-        $fullNameFromReduce = array_reduce($names, \Widmogrod\Functional\concatM, \Widmogrod\Monad\Maybe\Nothing::mempty());
+        $fullNameFromReduce = array_reduce($names, f\concatM, m\Nothing::mempty());
 
         $this->assertInstanceOf(Just::class, $fullName);
         $this->assertEquals($fullName->extract()->extract(), $expected);
