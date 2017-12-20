80430a6bcc382da9d2ed5cb66ddbee8205389882
widmogrod <widmogrod@gmail.com>
2017-12-20 18:58:48 +0100

    Functional\applicator tests

---
+++ test/Functional/ApplicatorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace test\Functional;
+
+use function Widmogrod\Functional\applicator;
+
+class ApplicatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function test_it_should_apply_value_as_a_argument_to_a_function(
+        $value,
+        callable $fn,
+        $expected
+    ) {
+        $this->assertSame(
+            $expected,
+            applicator($value, $fn)
+        );
+    }
+
+    /**
+     * @expectedException \ArgumentCountError
+     * @expectedExceptionMessage Too few arguments to function
+     */
+    public function test_it_should_fail_when_function_requires_more_argumetns()
+    {
+        applicator(1, function (int $i, string $a): int {
+            return 10 + $i;
+        });
+    }
+
+    public function provideData()
+    {
+        return [
+            'Single value function' => [
+                '$value' => 133,
+                '$fn' => function (int $i): int {
+                    return 10 + $i;
+                },
+                '$expected' => 143,
+            ],
+        ];
+    }
+}
