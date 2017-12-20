ba4f464fba9557da7e1d56c2b8dec1e55302e2da
widmogrod <widmogrod@gmail.com>
2017-12-20 21:58:22 +0100

    Functional\head test

---
+++ test/Functional/HeadTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace test\Functional;
+
+use Widmogrod\Primitive\Listt;
+use function Widmogrod\Functional\fromIterable;
+use function Widmogrod\Functional\head;
+
+class HeadTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function test_it_should_return_boxed_value(
+        Listt $listt,
+        $expected
+    ) {
+        $this->assertSame(
+            $expected,
+            head($listt)
+        );
+    }
+
+    public function provideData()
+    {
+        return [
+            'Should return head from finite array' => [
+                '$listt' => fromIterable([1, 2, 3]),
+                '$expected' => 1,
+            ],
+            'Should return head from finite iterator' => [
+                '$listt' => fromIterable(new \ArrayIterator([1, 2, 3])),
+                '$expected' => 1,
+            ],
+        ];
+    }
+
+    /**
+     * @expectedException \Widmogrod\Primitive\EmptyListError
+     * @expectedExceptionMessage Cannot call head() on empty list
+     */
+    public function test_it_should_throw_exception_when_list_is_empty()
+    {
+        head(Listt::mempty());
+    }
+}
