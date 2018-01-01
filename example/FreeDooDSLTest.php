97ecdd3142c4772de1cc002fd68c561c7dacd4a3
widmogrod <widmogrod@gmail.com>
2018-01-01 23:15:08 +0100

    Introduce contra example - how control flow looks like without do notation
    Introduce contr example - how control flow looks like without do notation

--- example/FreeDooDSLTest.php
+++ example/FreeDooDSLTest.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace example;
 
+use Widmogrod\Monad\Identity;
 use function Widmogrod\Monad\Control\Doo\doo;
 use function Widmogrod\Monad\Control\Doo\in;
 use function Widmogrod\Monad\Control\Doo\let;
-use Widmogrod\Monad\Identity;
 
 class FreeDooDSLTest extends \PHPUnit\Framework\TestCase
 {
-    public function test_it()
+    public function test_example_with_do_notation()
     {
         $result = doo(
             let('a', Identity::of(1)),
@@ -23,6 +23,23 @@ class FreeDooDSLTest extends \PHPUnit\Framework\TestCase
                 return Identity::of($c * $c);
             })
         );
+
+        $this->assertEquals(Identity::of(16), $result);
+    }
+
+    public function test_example_without_do_notation()
+    {
+        $result = Identity::of(1)
+            ->bind(function ($a) {
+                return Identity::of(3)
+                    ->bind(function ($b) use ($a) {
+                        return Identity::of($a + $b)
+                            ->bind(function ($c) {
+                                return Identity::of($c * $c);
+                            });
+                    });
+
+            });
 
         $this->assertEquals(Identity::of(16), $result);
     }
